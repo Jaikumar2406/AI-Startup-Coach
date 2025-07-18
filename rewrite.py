c8c5eb5c6c23998a7fcfc305903b84183dc76907
JAI KUMAR <155084229+Jaikumar2406@users.noreply.github.com>
2025-07-18 20:41:47 +0530

    rewrite.py

--- rewrite.py
+++ rewrite.py
@@ -8,7 +8,7 @@ from langchain_groq import ChatGroq
 load_dotenv()
 
 
-groq = os.getenv('groq')
+groq = os.getenv('GROQ')
 llm = ChatGroq(api_key=groq, temperature=0.5, model="llama-3.3-70b-versatile")
 
 
